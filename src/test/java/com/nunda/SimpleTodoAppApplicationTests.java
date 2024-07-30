a9335a9afa235af463feb6f598eab2fedddc12c3
NundaElroy <sekyezaelroy@gmail.com>
2024-07-30 08:38:08 +0300

    my first commit

---
+++ src/test/java/com/nunda/SimpleTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
