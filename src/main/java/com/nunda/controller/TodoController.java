6a31cbba2e726493a45e939c0eedd54d5e7858d6
NundaElroy <sekyezaelroy@gmail.com>
2024-08-01 17:06:52 +0300

    handling form data for both forms that is checkbox and item form

--- src/main/java/com/nunda/controller/TodoController.java
+++ src/main/java/com/nunda/controller/TodoController.java
@@ -3,7 +3,7 @@ package com.nunda.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import java.util.List;
 
@@ -21,8 +21,24 @@ public class TodoController{
     public String welcome(Model model){
         //getting the items sorted by there dates 
         List<TodoItem> items = todoService.getAllItems();
+        TodoItem todoItem = new TodoItem();
+        model.addAttribute("todoItem",todoItem);
         model.addAttribute("items", items);
         return "index";
+    }
+
+    @PostMapping("/new-item")
+    public String submitForm(@ModelAttribute TodoItem todoItem){
+         todoService.saveItem(todoItem);
+         return "redirect:";
+
+    }
+    @PostMapping("/updateStatus")
+    public String checkTodoItem(@RequestParam("itemId") Long itemId,@RequestParam("status") boolean status){
+        TodoItem todoItem = todoService.getItemById(itemId);
+        todoItem.setStatus(status);
+        todoService.saveItem(todoItem);
+        return "redirect:";
     }
 
 }
