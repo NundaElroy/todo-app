795d55e6c75f904d3d8dee6dcc9a2bf958a75ba6
NundaElroy <sekyezaelroy@gmail.com>
2024-08-01 17:08:40 +0300

    handling submission for the checkbox form

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -4,3 +4,13 @@ const formElement = document.getElementById("item_form");
 button.addEventListener('click', () => {
     formElement.classList.toggle('show');
 });
+
+//handling the form that deals with ticking the fields
+document.addEventListener('DOMContentLoaded', (event) => {
+    const checkbox = document.getElementById('statusCheckbox');
+    const form = document.getElementById('statusForm');
+
+    checkbox.addEventListener('change', function() {
+        form.submit();
+    });
+});
