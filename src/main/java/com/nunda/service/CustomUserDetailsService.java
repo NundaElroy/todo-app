642a0f487e7c1a8524af9fe9af089cdf1897dbce
NundaElroy <sekyezaelroy@gmail.com>
2024-08-28 02:20:35 +0300

    (new) email alerts and custom user service

---
+++ src/main/java/com/nunda/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.nunda.service;
+
+import com.nunda.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    public CustomUserDetailsService(UserService userService){
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles()
+                .build();
+    }
+
+}
