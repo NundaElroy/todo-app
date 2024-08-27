f4420b72a1118bf3d0fb0fe0881d5de5e6bc9e62
NundaElroy <sekyezaelroy@gmail.com>
2024-08-28 02:25:43 +0300

    (change) dependent on default auth provider instead of custom

--- src/main/java/com/nunda/config/SecurityConfig.java
+++ src/main/java/com/nunda/config/SecurityConfig.java
@@ -1,26 +1,32 @@
 package com.nunda.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-    private final AuthenticationProvider customAuthProvider;
 
-    public SecurityConfig(AuthenticationProvider customAuthProvider){
-        this.customAuthProvider =  customAuthProvider;
+    private   final  UserDetailsService userDetailsService;
+
+    public SecurityConfig( UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
     }
+
 
     @Bean
     public SecurityFilterChain web(HttpSecurity http) throws Exception {
 
         http
-                .authenticationProvider(customAuthProvider)
+                .userDetailsService(userDetailsService)
                 .authorizeHttpRequests((authorize) -> authorize
 
                         .requestMatchers("/features").permitAll()
@@ -52,4 +58,9 @@ public class SecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
