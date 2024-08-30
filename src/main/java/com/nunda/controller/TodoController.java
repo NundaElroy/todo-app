66e648dc3c2e0da6e979b02996d0a58a699ad0f9
NundaElroy <sekyezaelroy@gmail.com>
2024-08-30 22:33:50 +0300

    (change) handling the save issue for the items

--- src/main/java/com/nunda/controller/TodoController.java
+++ src/main/java/com/nunda/controller/TodoController.java
@@ -1,10 +1,14 @@
 package com.nunda.controller;
 
 
+import com.nunda.model.User;
+import com.nunda.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+
+import java.security.Principal;
 import java.util.List;
 
 import com.nunda.service.TodoService;
@@ -13,8 +17,14 @@ import com.nunda.model.TodoItem;
 @Controller
 public class TodoController{
 
-    @Autowired
-    private TodoService todoService;
+
+    private final TodoService todoService;
+    private final UserService userService;
+
+    public TodoController(TodoService todoService, UserService userService){
+        this.todoService = todoService;
+        this.userService = userService;
+    }
 
 
     @GetMapping({"","/"})
@@ -29,9 +39,12 @@ public class TodoController{
 
 
     @GetMapping("/home")
-    public String welcome(Model model){
-        //getting the items sorted by there dates 
-        List<TodoItem> items = todoService.getAllItems();
+    public String welcome(Model model, Principal principal){
+        //username of the logged in user
+        String email = principal.getName();
+        //getting the items sorted by there dates
+        User user = userService.findByEmail(email);
+        List<TodoItem> items = user.getTodoItems();
         TodoItem todoItem = new TodoItem();
         model.addAttribute("todoItem",todoItem);
         model.addAttribute("items", items);
@@ -39,23 +52,33 @@ public class TodoController{
     }
 
     @GetMapping("/archive")
-    public String getArchive(Model model){
-        List<TodoItem> allItemsArrangedByDate = todoService.getAllItemsByDate();
-        model.addAttribute("items",allItemsArrangedByDate);
+    public String getArchive(Model model,Principal principal){
+        //username of the logged in user
+        String email = principal.getName();
+        //getting the items sorted by there dates
+        //getting the items sorted by there dates
+        User user = userService.findByEmail(email);
+        List<TodoItem> items = user.getTodoItems();
+//      List<TodoItem> allItemsArrangedByDate = todoService.getAllItemsByDate();
+        model.addAttribute("items",items);
         return "archive";
     }
 
     @PostMapping("/new-item")
-    public String submitForm(@ModelAttribute TodoItem todoItem){
-         todoService.saveItem(todoItem);
-         return "redirect:";
+    public String submitForm(@ModelAttribute TodoItem todoItem,Principal principal){
+        String username = principal.getName();
+         User user = userService.findByEmail(username);
+         userService.addToTodoItemsList(user,todoItem);
+         return "redirect:/home";
     }
+
+
     @PostMapping("/updateStatus")
     public String checkTodoItem(@RequestParam("itemId") Long itemId,@RequestParam("status") boolean status){
         TodoItem todoItem = todoService.getItemById(itemId);
         todoItem.setStatus(status);
         todoService.saveItem(todoItem);
-        return "redirect:";
+        return "redirect:/home";
     }
 
 
